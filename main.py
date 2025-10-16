8ae6278c7b0e4f2f1b12bef557b7f6f6993aca9d
pragatipatel26 <pragatipatel2606@gmail.com>
2025-10-17 01:42:42 +0530

    Removed .env file and added to .gitignore

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import extract
 
 load_dotenv()
 tavily_key = os.getenv("TAVILY_API_KEY")
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools=[TavilySearch()]
 llm=ChatGroq(model="llama-3.3-70b-versatile")
+structured_llm=llm.with_structured_output(AgentResponse)
 react_prompt=hub.pull("hwchase17/react")
+react_prompt_with_format_instructions=PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input","agent_scratchpad","tool_names"],
+).partial(format_instructions="")
+
 agent=create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt
+    prompt=react_prompt_with_format_instructions
 )
 agent_executor=AgentExecutor(agent=agent,tools=tools,verbose=True)
-chain=agent_executor
+extract_output=RunnableLambda(lambda x:x["output"])
+chain=agent_executor|extract_output|structured_llm
 
 def main():
     result = chain.invoke(
